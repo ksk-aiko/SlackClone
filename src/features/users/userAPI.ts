f4e1b0668f68a130cc187f35fef1319fe544eb77
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-01-02 13:00:46 +0900

    Add user API functions and User type definition; implement getUser function to fetch user data from Firestore

---
+++ src/features/users/userAPI.ts
@@ -0,0 +1,26 @@
+/**
+ * 指定されたユーザーUIDのユーザー情報を取得する非同期関数
+ * 
+ * @param {string} user_uid - ユーザーのUID
+ * @returns {Promise<User | undefined>} ユーザー情報を含むPromise。ドキュメントが存在しない場合はundefinedを返す。
+ */
+import {doc, getDoc, getFirestore} from 'firebase/firestore';
+import {firebaseApp} from '../../firebase/firebaseConfig';
+import {User} from '../../type/User';
+
+// Firestoreデータベースのインスタンスを取得
+const db = getFirestore(firebaseApp);
+
+// ユーザー情報を取得する関数
+export const getUser = async(user_uid: string) => {
+    // 指定されたユーザーUIDのドキュメント参照を取得
+    const userRef = doc(db, 'users', user_uid);
+    // ドキュメントのスナップショットを取得
+    const docSnap = await getDoc(userRef);
+    // ドキュメントが存在する場合、データをUser型として返す
+    if (docSnap.exists()) {
+        return docSnap.data() as User;
+    }
+}
+
+
---
+++ src/type/User.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents a user in the SlackClone application.
+ */
+export interface User {
+    profile_picture: string;
+    email: string;
+    displayName: string;
+}
+
+/**
+ * ユーザー参照を表すインターフェース。
+ * 
+ * @property {string} uid - ユーザーの一意の識別子。
+ * @property {User} user - ユーザーオブジェクト。
+ */
+export interface UserRef {
+    uid: string;
+    user: User;
+}
