22889e279faff3f94d42931628a2fd33f9a6f10b
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-01-10 17:42:05 +0900

    Manage login status based on user ID and conditionally switch the display of the sidebar and chat container.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import './App.css';
 import SideBar from "./components/SideBar.tsx";
 import ChatContainer from "./components/ChatContainer.tsx";
 import Login from './components/Login.tsx';
+import { useAppSelector } from './app/hook.ts';
 
 function App() {
+  const userId = useAppSelector((state) => state.user.userId);
   return (
     <div className="flex">
-      <Login />
-      {/* <SideBar />
-      <ChatContainer /> */}
+      {userId ? (
+        <>
+        <SideBar />
+        <ChatContainer />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
