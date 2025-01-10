8b5a1c395ef4d68c7f7f4693c53176b7d0712b3e
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-01-10 15:27:47 +0900

    Updated the login component to add a function that sets the user ID to the Redux store when logging in with Google; wrapped the entire app in a Redux provider

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import {signInWithGoogle} from '../features/auth/Auth.ts';
 import {getUser} from '../features/users/userAPI.ts';
+import {useAppDispatch} from '../app/hook.ts';
+import {login} from '../features/users/userSlice.ts';
 
 const Login = () => {
-    const getUserInfo = async () => {
-        try {
-            const user = getUser('DQAWv70Bka9DZBIZX9Ww');
-            if (user) {
-                console.log(user);
-            }
-        } catch (error) {
-            console.log('Login failed:', error);
-        }
-    }
-    const loginWithGoogle = async () => {
-        try {
-            const result = await signInWithGoogle();
-            const login_user = result.user;
-            console.log(login_user);
-        } catch (error) {
-            console.log('Login failed;', error);
-        }
-    }
+    const dispatch = useAppDispatch();
+    
+    const setUserId = () => {
+        dispatch(login("DQAWv70Bka9DZBIZX9Ww"))
+    };
+    // const getUserInfo = async () => {
+    //     try {
+    //         const user = getUser('DQAWv70Bka9DZBIZX9Ww');
+    //         if (user) {
+    //             console.log(user);
+    //         }
+    //     } catch (error) {
+    //         console.log('Login failed:', error);
+    //     }
+    // }
+    // const loginWithGoogle = async () => {
+    //     try {
+    //         const result = await signInWithGoogle();
+    //         const login_user = result.user;
+    //         console.log(login_user);
+    //     } catch (error) {
+    //         console.log('Login failed;', error);
+    //     }
+    // }
 
     return (
         <div className="fixed flex inset-0 items-center justify-center bg-gray-500">
@@ -33,7 +40,7 @@ const Login = () => {
                     <div className="flex items-center justify-center">
                         <button 
                         className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        onClick={getUserInfo}>
+                        onClick={setUserId}>
                         ログイン
                         </button>
                     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.ts';
 import { Provider } from 'react-redux';
-import store from './app/store.ts';
+import {store} from './app/store.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
