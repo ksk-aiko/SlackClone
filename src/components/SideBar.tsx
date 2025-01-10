60c67db2a395dc296b46efd188c78f7ba850e358
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-01-10 18:18:28 +0900

    We added a function to the sidebar component to obtain user information, and display names based on user IDs.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import {Home, ChatBubble} from "@mui/icons-material";
+import { useAppSelector} from '../app/hook.ts';
+import { getUser } from '../features/users/userAPI.ts';
+import { User } from '../type/User.ts';
 
 const SideBar = () => {
+  const userId = useAppSelector((state) => state.user.userId);
+  const [user, setUser] = useState<User | null>();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userId) {
+        const userRef = await getUser(userId);
+        if (userRef) {
+          setUser(userRef);
+        }
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
   return (
     <div className="w-16 h-screen bg-gray-900 text-white">
       <div className="py-5 flex flex-col items-center">
@@ -19,6 +38,7 @@ const SideBar = () => {
         <div className="bg-gray-700 p-2 rounded-lg">
           <img src={"/default-user-icon.webp"} alt="" />
         </div>
+        <span className="text-xs">{user?.displayName}</span>
       </div>
     </div>
   );
