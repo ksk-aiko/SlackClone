89569afc0a4befa6d05640079ebecc44d01d811e
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-03-01 20:50:03 +0900

    Add DMChat and DMMessage interfaces for direct messaging functionality

---
+++ src/type/DM.ts
@@ -0,0 +1,33 @@
+import { TimeStamp } from 'firebase/firestore';
+
+export interface DMChat {
+    id?: string;
+    participants: string[];
+    created_at: TimeStamp;
+    updated_at: TimeStamp;
+    last_message?: {
+        text: string;
+        sender_id: string;
+        sent_at: TimeStamp;
+    }
+}
+
+export interface DMMessage {
+    user_id: string;
+    dm_chat_id: string;
+    text: string;
+    created_at: TimeStamp;
+    is_edited: boolean;
+    updated_at: TimeStamp;
+    is_read: boolean; 
+}
+
+export interface DMMessageRef {
+    id: string;
+    message: DMMessage;
+}
+
+export interface DMChatRef {
+    id: string;
+    chat: DMChat;
+}
