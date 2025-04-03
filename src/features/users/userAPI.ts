ca0b1c8ae03c2b4f1b9a8edd51f123d66415cb62
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-04-03 09:27:25 +0900

    Add fetchUsers function to retrieve all users from Firestore

--- src/features/users/userAPI.ts
+++ src/features/users/userAPI.ts
@@ -4,7 +4,7 @@
  * @param {string} user_uid - User UID
  * @returns {Promise<User | undefined>} Promise containing user information. If no documentation exists, undefined is returned.
  */
-import {doc, getDoc, setDoc, getFirestore} from 'firebase/firestore';
+import {doc, getDoc, getDocs, setDoc, getFirestore, collection} from 'firebase/firestore';
 import {firebaseApp} from '../../firebase/firebaseConfig.ts';
 import {User, UserRef} from '../../type/User';
 
@@ -31,5 +31,27 @@ export const postUser = async (userRef:UserRef) => {
         profile_picture: user.profile_picture
     });
 };
+
+// Function to fetch all users
+export const fetchUsers = async (): Promise<UserRef[]> => {
+    try {
+        const usersCollection = collection(db, 'users');
+        const usersSnapshot = await getDocs(usersCollection);
+
+        const userList: UserRef[] = [];
+
+        usersSnapshot.forEach((doc) => {
+            userList.push({
+                uid: doc.id,
+                user: doc.data() as User
+            });
+        });
+
+        return userList;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw error;
+    }
+}
 
 
